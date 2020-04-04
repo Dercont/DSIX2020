841dc67ef1acbe9b00755270e95a279277ec255e
Dercont <armando.almengor@utp.ac.pa>
2020-04-04 14:51:48 -0500

    Restricción de Caracteres

--- Diag_Test/encoder.js
+++ Diag_Test/encoder.js
@@ -10,38 +10,42 @@ const rl = readline.createInterface({
 });
 
 rl.question("What is your string (1~10 chars) to encode? ", function (encode) {
-    rl.question("Press 0 for BASE64, 1 for ROT13, 2 for HEX, 3 for BINARY ", function (enc_type) {
+    if (encode.length <= 10) {
+        rl.question("Press 0 for BASE64, 1 for ROT13, 2 for HEX, 3 for BINARY ", function (enc_type) {
 
-   // if (enc_type == )    
-        switch (enc_type) {
+            switch (enc_type) {
 
-            case '0':
-                console.log(`Your result is, ${encodelib.encode(encode, 'base64')}`);
-                console.log(`Encode type selected: BASE64`);
-                break;
+                case '0':
+                    console.log(`Your result is, ${encodelib.encode(encode, 'base64')}`);
+                    console.log(`Encode type selected: BASE64`);
+                    break;
 
-            case '1':
-                console.log(`Your result is, ${rot13x(encode)}`);
-                console.log(`Encode type selected: ROT13`);
-                break;
-            
-            case '2':
-                console.log(`Your result is, ${encodelib.encode(encode, 'hex')}`);
-                console.log(`Encode type selected: HEX`);
-                break;
+                case '1':
+                    console.log(`Your result is, ${rot13x(encode)}`);
+                    console.log(`Encode type selected: ROT13`);
+                    break;
 
-            case '3':
-                console.log(`Your result is, ${binaryx.encode(encode)}`);
-                console.log(`Encode type selected: BINARY`);
-                break;
-            
-            default:
-                console.log("Wrong parameter.");  
-        }
+                case '2':
+                    console.log(`Your result is, ${encodelib.encode(encode, 'hex')}`);
+                    console.log(`Encode type selected: HEX`);
+                    break;
 
+                case '3':
+                    console.log(`Your result is, ${binaryx.encode(encode)}`);
+                    console.log(`Encode type selected: BINARY`);
+                    break;
+
+                default:
+                    console.log("Wrong parameter.");
+            }
+
+            rl.close();
+        });
+    } else {
+        console.log("String too long, try again!")
         rl.close();
+    }
 
-    });
 });
 
 rl.on("close", function () {
